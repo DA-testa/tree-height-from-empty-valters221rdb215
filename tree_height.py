d59a61a08904710894e623dac2b79f6e693ca9dc
valters221rdb215 <125762443+valters221rdb215@users.noreply.github.com>
2023-03-05 21:51:50 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,53 +1,54 @@
 import sys
 import threading
 import numpy as np
-
+ 
 def compute_tree_height(n, prnts):
     chldrn = {i: [] for i in range(n)}
-    root = []
+    root = [] 
 
     for i, parent in enumerate(prnts):
         if parent == -1:
             root.append(i)
-        else:
+        else: 
             chldrn[parent].append(i)
-
+ 
+            
     def find_max_depth(node, d):
         if not chldrn[node]:
-            return d
+            return d 
         else:
-            max_D = 0
+            max_D = 0 
             for child in chldrn[node]:
-                child_D = find_max_depth(child, d+1)
+                child_D = find_max_depth(child, d+1) 
                 max_D = max(max_D, child_D)
-            return max_D
-
-    max_H = 0
+            return max_D 
+ 
+    max_H =  0 
     for r in root:
         treeheight = find_max_depth(r, 0)
-        max_H = max(max_H, treeheight)
+        max_H = max(max_H, treeheight) 
 
-    return max_H + 1
+    return max_H  + 1
 
-def main():
+def main(): 
     burts = input()
-
+ 
     if 'I' in burts:
-        n = int(input())
+        n = int(input()) 
         prnts = list(map(int, input().split()))
         print(compute_tree_height(n, prnts))
 
-    elif 'F' in burts:
+    elif 'F' in burts: 
         fails = input()
 
         if 'a' in fails:
-            return
-
-        path = "test/" + fails
-        with open(path, 'r') as v:
+            return 
+  
+        path = "test/"+ fails
+        with open(path, 'r') as v: 
             n = int(v.readline())
             prnts = list(map(int, v.readline().split()))
-            print(compute_tree_height(n, prnts))
+            print(compute_tree_height(n, prnts)) 
 
 sys.setrecursionlimit(10**7) 
 threading.stack_size(2**27)
