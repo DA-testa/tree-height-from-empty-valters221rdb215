81e51e1eb057fd87060f9c1901baf16a244f4c23
valters221rdb215 <125762443+valters221rdb215@users.noreply.github.com>
2023-03-05 20:01:36 +0200

    Create tree_height.py

---
+++ tree_height.py
@@ -0,0 +1,48 @@
+import sys
+import threading
+import numpy
+
+def compute_height(num_nodes, parents):
+    tree = numpy.zeros(num_nodes)
+
+    def get_height(node):
+        if tree[node] != 0:
+            return tree[node]
+
+        if parents[node] == -1:
+            tree[node] = 1
+        else:
+            tree[node] = 1 + get_height(parents[node])
+
+        return tree[node]
+
+    for node in range(num_nodes):
+        get_height(node)
+
+    return int(max(tree))
+
+
+def main():
+    file_name = input("Enter file name: ").strip()
+
+    while 'a' in file_name or 'A' in file_name:
+        file_name = input("Invalid file name. Enter again: ").strip()
+
+    try:
+        with open(f"./{file_name}") as file:
+            num_nodes = int(file.readline().strip())
+            parents = list(map(int, file.readline().split()))
+
+        result = compute_height(num_nodes, parents)
+
+        with open(f"./{file_name.split('.')[0]}_output.txt", 'w') as file:
+            file.write(str(result))
+
+    except FileNotFoundError:
+        print("File not found.")
+
+
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
+
+threading.Thread(target=main).start()
