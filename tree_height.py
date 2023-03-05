13fc9c12782b837598f5973864cced191c1a947c
valters221rdb215 <125762443+valters221rdb215@users.noreply.github.com>
2023-03-05 20:28:56 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,38 +2,42 @@ import sys
 import threading
 import numpy
 
+class Node:
+    def __init__(self, index):
+        self.index = index
+        self.children = []
+
+
 def compute_height(n, parents):
-    children = {i: [] for i in range(n)}
-    
+    nodes = [Node(i) for i in range(n)]
+    root = None
     for i in range(n):
-        parent = parents[i]
-        if parent != -1:
-            children[parent].append(i)
-    
-    def get_height(node):
-        if children[node] == []:
-            return 1
+        parent_index = parents[i]
+        if parent_index == -1:
+            root = nodes[i]
         else:
-            heights = [get_height(child) for child in children[node]]
-            return max(heights) + 1
-    
-    root = parents.index(-1)
+            nodes[parent_index].children.append(nodes[i])
     height = get_height(root)
-    
     return height
+
+
+def get_height(node):
+    if not node.children:
+        return 1
+    max_child_height = 0
+    for child in node.children:
+        child_height = get_height(child)
+        max_child_height = max(max_child_height, child_height)
+    return max_child_height + 1
+
 
 def main():
     n = int(input())
     parents = list(map(int, input().split()))
-    
+
     height = compute_height(n, parents)
-    
     print(height)
 
-if __name__ == '__main__':
-    sys.setrecursionlimit(10**7)
-    threading.stack_size(2**27)
-    
-    thread = threading.Thread(target=main)
-    thread.start()
-    thread.join()
+sys.setrecursionlimit(10*7)
+threading.stack_size(2*27) 
+threading.Thread(target=main).start()
