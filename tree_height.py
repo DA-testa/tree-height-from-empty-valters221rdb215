dfe0039ea5e69f67c5c22fe6983b52c69f734d7a
valters221rdb215 <125762443+valters221rdb215@users.noreply.github.com>
2023-03-05 20:21:34 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,47 +2,38 @@ import sys
 import threading
 import numpy
 
-def compute_height(num_nodes, parents):
-    tree = numpy.zeros(num_nodes)
-
+def compute_height(n, parents):
+    children = {i: [] for i in range(n)}
+    
+    for i in range(n):
+        parent = parents[i]
+        if parent != -1:
+            children[parent].append(i)
+    
     def get_height(node):
-        if tree[node] != 0:
-            return tree[node]
-
-        if parents[node] == -1:
-            tree[node] = 1
+        if children[node] == []:
+            return 1
         else:
-            tree[node] = 1 + get_height(parents[node])
-
-        return tree[node]
-
-    for node in range(num_nodes):
-        get_height(node)
-
-    return int(max(tree))
-
+            heights = [get_height(child) for child in children[node]]
+            return max(heights) + 1
+    
+    root = parents.index(-1)
+    height = get_height(root)
+    
+    return height
 
 def main():
-    file_name = input().strip()
+    n = int(input())
+    parents = list(map(int, input().split()))
+    
+    height = compute_height(n, parents)
+    
+    print(height)
 
-    while 'a' in file_name or 'A' in file_name:
-        file_name = input().strip()
-
-    try:
-        with open(f"./{file_name}") as file:
-            num_nodes = int(file.readline().strip())
-            parents = list(map(int, file.readline().split()))
-
-        result = compute_height(num_nodes, parents)
-
-        with open(f"./{file_name.split('.')[0]}_output.txt", 'w') as file:
-            file.write(str(result))
-
-    except FileNotFoundError:
-        pass
-
-
-sys.setrecursionlimit(10**7)
-threading.stack_size(2**27)
-
-threading.Thread(target=main).start()
+if __name__ == '__main__':
+    sys.setrecursionlimit(10**7)
+    threading.stack_size(2**27)
+    
+    thread = threading.Thread(target=main)
+    thread.start()
+    thread.join()
