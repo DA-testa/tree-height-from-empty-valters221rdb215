470cf789acd2c7f66e4872658a46b652f13f6b21
valters221rdb215 <125762443+valters221rdb215@users.noreply.github.com>
2023-03-05 20:33:55 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,42 +2,55 @@ import sys
 import threading
 import numpy
 
-class Node:
-    def __init__(self, index):
-        self.index = index
-        self.children = []
-
-
 def compute_height(n, parents):
-    nodes = [Node(i) for i in range(n)]
-    root = None
-    for i in range(n):
-        parent_index = parents[i]
-        if parent_index == -1:
-            root = nodes[i]
+    children = {i: [] for i in range(n)}
+    roots = []
+
+    for i, parent in enumerate(parents):
+        if parent == -1:
+            roots.append(i)
         else:
-            nodes[parent_index].children.append(nodes[i])
-    height = get_height(root)
-    return height
+            children[parent].append(i)
 
+    def find_max_depth(node, depth):
+        if not children[node]:
+            return depth
+        else:
+            max_depth = 0
+            for child in children[node]:
+                child_depth = find_max_depth(child, depth+1)
+                max_depth = max(max_depth, child_depth)
+            return max_depth
 
-def get_height(node):
-    if not node.children:
-        return 1
-    max_child_height = 0
-    for child in node.children:
-        child_height = get_height(child)
-        max_child_height = max(max_child_height, child_height)
-    return max_child_height + 1
+    max_height = 0
+    for root in roots:
+        height = find_max_depth(root, 0)
+        max_height = max(max_height, height)
+
+    return max_height + 1
 
 
 def main():
-    n = int(input())
-    parents = list(map(int, input().split()))
+    letter = input()
 
-    height = compute_height(n, parents)
-    print(height)
+    if 'I' in letter:
+        n = int(input())
+        parents = list(map(int, input().split()))
+        print(compute_height(n, parents))
 
-sys.setrecursionlimit(10*7)
-threading.stack_size(2*27) 
+    elif 'F' in letter:
+        file_name = input()
+
+        if 'a' in file_name:
+            return
+
+        file_path = "test/" + file_name
+        with open(file_path, 'r') as f:
+            n = int(f.readline())
+            parents = list(map(int, f.readline().split()))
+            print(compute_height(n, parents))
+
+
+sys.setrecursionlimit(10**7) 
+threading.stack_size(2**27)
 threading.Thread(target=main).start()
